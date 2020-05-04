017e141f3744c889603e699bdb5b799ffb44983d
steph <stephanmettler@gmx.de>
2020-05-04 05:01:34 +0200

    adx

--- lib/bitmex-wss/lib/orderbook.ts
+++ lib/bitmex-wss/lib/orderbook.ts
@@ -9,7 +9,7 @@ const MAS_ask_vol = MAs([5, 15, 60]); // 5m, 15m, 60m
 const MAS_bid_vol = MAs([5, 15, 60]); // 5m, 15m, 60m
 
 export const onBookReplace = ({ data, symbol, tableName, writeApi }: any) => {
-  const bucketSize = 2;
+  const bucketSize = 5;
   const book = { Buy: {}, Sell: {} }; // unmodified values
   //prettier-ignore
   let askVol = 0, bidVol = 0, hAsk = 0, lBid = 9999999999;
@@ -19,7 +19,6 @@ export const onBookReplace = ({ data, symbol, tableName, writeApi }: any) => {
     const rate = Math.ceil(price / bucketSize) * bucketSize;
     if (!book[side][rate]) book[side][rate] = size;
     else book[side][rate] = book[side][rate] + size;
-
     if (side === 'Buy') {
       askVol += size;
       hAsk = Math.max(hAsk, price);
@@ -31,7 +30,6 @@ export const onBookReplace = ({ data, symbol, tableName, writeApi }: any) => {
   const asks = Object.keys(book.Buy).map((k) => [Number(k), book.Buy[k]]);
   //prettier-ignore
   const bids = Object.keys(book.Sell).map((k) => [Number(k), book.Sell[k]]).reverse();
-
   const askVolAvg = maAskVol.push(askVol); // calc avg
   const bidVolAvg = maBidVol.push(bidVol); // calc avg
 
@@ -46,14 +44,13 @@ export const onBookReplace = ({ data, symbol, tableName, writeApi }: any) => {
     bidVolAvg,
     hAsk,
     lBid,
-    diff1: volReduce(asks, 1) - volReduce(bids, 1),
     diff3: volReduce(asks, 3) - volReduce(bids, 3),
     diffN: askVol - bidVol,
   };
 
   console.log({
     data: { hAsk, lBid, book: [asks.length, bids.length] },
-    diffsss: { diff1: stats.diff1, diff3: stats.diff3, diffN: stats.diffN },
+    volDiffs: { diff3: stats.diff3, diffN: stats.diffN },
     askAvgs: {
       askVol,
       askVolAvg,
@@ -66,25 +63,29 @@ export const onBookReplace = ({ data, symbol, tableName, writeApi }: any) => {
     },
     // MAS_ask_vol: stats.MAS_ask_vol,
     // MAS_bid_vol: stats.MAS_bid_vol,
-
     Guess: guess(stats),
   });
 
   writeBookPoints(writeApi, stats);
 };
 
+// data: { hAsk: undefined, book: [ 7, 7 ] },
+// diffsss: { diff1: -1306, diff3: -1469, diffN: -3457516 },
+// askAvgs: { askVol: 1553961, askVolAvg: 2875942, diff: -1321981 },
+// bidAvgs: { bidVol: 5011477, bidVolAvg: 3034249, diff: 1977228 },
+
+//
+
 let signal;
-const guess = ({ diff1, diff3, diffN, askVol, bidVol, askVolAvg, bidVolAvg }) => {
+const guess = ({ diff3, diffN, askVol, bidVol, askVolAvg, bidVolAvg }) => {
   let _signal = 'FLAT';
-  if (diff1 < 0 && diff3 < 0 && diffN < 0 && askVol < bidVol) _signal = 'SHORT';
-  else if (diff1 > 0 && diff3 > 0 && diffN > 0 && askVol > bidVol) _signal = 'LONG';
+  if (diff3 < 0 && diffN < 0 && askVol < bidVol) _signal = 'SHORT';
+  else if (diff3 > 0 && diffN > 0 && askVol > bidVol) _signal = 'LONG';
   if (_signal !== signal) alert(_signal);
   signal = _signal;
   return _signal;
 };
 
-const highestAsk = (asks) => asks.slice(-1)[0];
-const lowestBid = (bids) => bids.slice(-1)[0];
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 // prettier-ignore
 const volReduce = (book, volDepth) =>Math.round(book.slice(volDepth * -1).map((d) => d[1]).reduce(reducer, 0)/1000);
--- lib/indicator/indicator2.ts
+++ lib/indicator/indicator2.ts
@@ -1,15 +1,13 @@
 import { candler } from '../query';
-//import { CrossUp } from 'technicalindicators/lib/Utils/CrossUp';
+import { ADX, EMA, SMA, ema, sma, CrossDown, CrossUp } from 'technicalindicators'; //technicalIndicators.setConfig('precision', 7);
 const _MA = require('moving-average');
-const technicalIndicators = require('technicalindicators');
-
-//const CrossUp = require('technicalindicators/lib/Utils/CrossUp').CrossUp;
-
-const { EMA, SMA, ema, sma } = technicalIndicators; // reversedInput : true
-technicalIndicators.setConfig('precision', 7);
-const beep = require('beepbeep'); //screen: CTRL-A CTRL-G
-import { exec } from 'child_process';
-
+import { exec } from 'child_process'; //screen: CTRL-A CTRL-G
+type InputType = {
+  close: number[];
+  high: number[];
+  low: number[];
+  period: number;
+};
 export const MA = (interval = 5) => {
   const _ma = _MA(interval * 60 * 1000); // track moving-average - default: 5m
   return {
@@ -58,27 +56,58 @@ export const alert = (type) => {
   else if (type === 'FLAT') exec('beep -l 100 -f 500');
 };
 
-// alertTerminal();
-// process.stdout.write('\x07');
-// setTimeout(() => console.log('\x07'), 1250);
-// beep();
-//beep([1000, 500, 2000]);
-//beep(3, 1000);
-
-const calcTrends = async ({ emas = [10, 20] }: { emas?: number[] }) => {
+const calcTrends = async ({ emas = [9, 20] }: { emas?: number[] }) => {
   const results = [];
   const ema_f = new EMA({ period: emas[0], values: [] });
   const ema_s = new EMA({ period: emas[1], values: [] });
   const ema_fast = [];
   const ema_slow = [];
 
+  let input: InputType = {
+    close: [],
+    high: [],
+    low: [],
+    period: 14,
+  };
   const rowHandler = ({ open, high, low, close, volume, trades, vwap, timestamp }) => {
     let f = ema_f.nextValue(close);
     let s = ema_s.nextValue(close);
     if (f) ema_fast.push(Math.round(f));
     if (s) ema_slow.push(Math.round(s));
+    input.close.push(close);
+    7;
+    input.high.push(high);
+    input.low.push(low);
   };
-  let a = await candler({ symbol: 'XBTUSD', binSize: '1m', start: '-1h', rowHandler });
-  console.log({ fast: ema_fast.slice(-10), slow: ema_slow.slice(-10) });
+  let ca = await candler({ symbol: 'XBTUSD', binSize: '1m', start: '-1h', rowHandler });
+  // console.log(CrossDown.calculate({ lineA: ema_fast, lineB: ema_slow }));
+  // console.log(CrossUp.calculate({ lineA: ema_fast, lineB: ema_slow }));
+  var adx = new ADX(input);
+  let res = adx.getResult();
+  console.log(res);
 };
 calcTrends({});
+
+var input = {
+  lineA: [7, 6, 5, 4, 3, 8, 3, 5, 3, 8, 5, 5, 3, 8, 5, 5, 8, 3],
+  lineB: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+};
+
+//var expectedResults = [false, false, false, true, false, false, true, false, false, false, false, false, true, false, false, false, false, true];
+
+//var crossDown = new CrossDown({ lineA: [], lineB: [] });
+
+const calcAdx = (data: any) => {
+  var adx = new ADX({ period: 15, high: [], low: [], close: [] });
+  var results = [];
+
+  var input: InputType = {
+    close: [29.87, 30.24, 30.1, 28.9, 28.92, 28.48, 28.56, 27.56, 28.47, 28.28, 27.49, 27.23, 26.35, 26.33, 27.03, 26.22, 26.01, 25.46, 27.03, 27.45, 28.36, 28.43, 27.95, 29.01, 29.38, 29.36, 28.91, 30.61, 30.05, 30.19, 31.12, 30.54, 29.78, 30.04, 30.49, 31.47, 32.05, 31.97, 31.13, 31.66, 32.64, 32.59, 32.19, 32.1, 32.93, 33.0, 31.94],
+    high: [30.2, 30.28, 30.45, 29.35, 29.35, 29.29, 28.83, 28.73, 28.67, 28.85, 28.64, 27.68, 27.21, 26.87, 27.41, 26.94, 26.52, 26.52, 27.09, 27.69, 28.45, 28.53, 28.67, 29.01, 29.87, 29.8, 29.75, 30.65, 30.6, 30.76, 31.17, 30.89, 30.04, 30.66, 30.6, 31.97, 32.1, 32.03, 31.63, 31.85, 32.71, 32.76, 32.58, 32.13, 33.12, 33.19, 32.52],
+    low: [29.41, 29.32, 29.96, 28.74, 28.56, 28.41, 28.08, 27.43, 27.66, 27.83, 27.4, 27.09, 26.18, 26.13, 26.63, 26.13, 25.43, 25.35, 25.88, 26.96, 27.14, 28.01, 27.88, 27.99, 28.76, 29.14, 28.71, 28.93, 30.03, 29.39, 30.14, 30.43, 29.35, 29.99, 29.52, 30.94, 31.54, 31.36, 30.92, 31.2, 32.13, 32.23, 31.97, 31.56, 32.21, 32.63, 31.76],
+    period: 14,
+  };
+  var adx = new ADX(input);
+  let res = adx.getResult();
+  console.log(res);
+};
